2a8be997f75aa462d7de31c1112aadc4e259a530
Brian Hicks <brian@brianthicks.com>
2022-01-27 07:44:02 -0600

    switch to use Naersk to build

--- default.nix
+++ default.nix
@@ -1,23 +1,11 @@
-{ sources ? import ./nix/sources.nix { }, pkgs ? import sources.nixpkgs { }
-, cargoSha256 ? "RSvkJhXS4neUaNVaHPRjEGqW2/pb51FbNtCoX2a4ePs=", ... }:
+{ sources ? import ./nix/sources.nix { }, pkgs ? import sources.nixpkgs { }, ...
+}:
 with pkgs;
-let gitignore = pkgs.callPackage sources.gitignore { };
-in rustPlatform.buildRustPackage rec {
-  pname = "firstaide";
-  version = "0.1.6";
+let
+  gitignore = pkgs.callPackage sources.gitignore { };
+  naersk = pkgs.callPackage sources.naersk { };
+in naersk.buildPackage {
   src = gitignore.gitignoreSource ./.;
-
-  # The crypto_hash crate needs the openssl-sys crate (directly or indirectly,
-  # I don't know) which ultimately needs openssl proper, and pkg-config.
-  buildInputs = [ openssl pkg-config ];
-
-  # Don't run tests when building.
-  doCheck = false;
-
-  # I think this refers to the current state of the crates.io repo. To update,
-  # replace the hash with all 0's and Nix will give you the right value to
-  # stick in here.
-  inherit cargoSha256;
 
   meta = with pkgs.lib; {
     description = "Bootstrap Nix environments.";
